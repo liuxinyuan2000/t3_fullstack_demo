0d95b3e5f0f5eb883717fb66b8a8b5b35c7e52e2
1018538830@qq.com <1018538830@qq.com>
2023-03-12 20:27:13 +0800

    提交demo

---
+++ src/server/api/routers/guestbook.ts
@@ -0,0 +1,43 @@
+// src/server/api/routers/guestbook.ts
+
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+
+
+export const guestbookRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    try {
+      return await ctx.prisma.guestbook.findMany({
+        select: {
+          name: true,
+          message: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
+  }),
+  
+  postMessage: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        message: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.guestbook.create({
+          data: {
+            name: input.name,
+            message: input.message,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+});
